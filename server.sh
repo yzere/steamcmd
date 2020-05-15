7b8f77671fe6abc8cd41d724f71fdc386f0aa65c
yzere <50502103+yzere@users.noreply.github.com>
2020-05-15 12:02:04 +0200

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install docker.iodocker 
+docker pull yzere/steamcmd:latest
+docker run -it -d --name=steamcmd yzere/steamcmd bash
+docker exec -it steamcmd bash
+/bin/bash ./steamcmd.sh +login anonymous +app_update 343050 +quit
+curl -o ~/run_dedicated_server.sh https://github.com/yzere/steamcmd/blob/master/run_dedicated_server.sh
+chmod u+x ~/run_dedicated_server.sh
+mkdir ~/.klei/
+mkdir ~/.klei/DoNotStarveTogether/
+echo "Now upload MyDediServer.zip and unpack in .klei/DoNotStarveTogether/, after this run ~/run_dedicated_server.sh"
